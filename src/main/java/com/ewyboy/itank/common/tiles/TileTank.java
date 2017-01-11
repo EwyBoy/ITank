9afeb0839ee41b211e667510ad65e507f1ecc810
Thefjong <Mdsoelvberg@gmail.com>
2017-01-11 04:30:48 +0100

    *Added the Tesla Maven *On fluid fill, the fluid will find the bottom tank and fill that

--- src/main/java/com/ewyboy/itank/common/tiles/TileTank.java
+++ src/main/java/com/ewyboy/itank/common/tiles/TileTank.java
@@ -1,6 +1,7 @@
 package com.ewyboy.itank.common.tiles;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
@@ -20,16 +21,26 @@ public class TileTank extends FluidTank {
     }
 
     @Override
-    public boolean canFillFluidType (FluidStack fluid) {
+    public boolean canFillFluidType(FluidStack fluid) {
         return super.canFillFluidType(fluid);
     }
 
     @Override
-    public void onContentsChanged () {
+    public void onContentsChanged() {
         if (this.tile != null) {
             final IBlockState state = this.tile.getWorld().getBlockState(this.tile.getPos());
             this.tile.getWorld().notifyBlockUpdate(this.tile.getPos(), state, state, 8);
             this.tile.markDirty();
         }
+    }
+
+    @Override
+    public int fill(FluidStack resource, boolean doFill) {
+        TileEntity tileDown = tile.getWorld().getTileEntity(tile.getPos().down());
+        if (tileDown instanceof TileEntityTank) {
+            int tankFill = ((TileEntityTank) tileDown).tank.fill(resource, doFill);
+            return tankFill != 0 ? tankFill : super.fill(resource, doFill);
+        }
+        return super.fill(resource, doFill);
     }
 }
